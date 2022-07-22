5a12dc1f2578552183759c13f9553b8af59d7b23
mircheus <mircheuss@yandex.ru>
2022-07-22 04:09:57 +0300

    space deletion

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
-
 public class EnemySpawner : MonoBehaviour
 {
     [SerializeField] private int _repeatRate = 2;
